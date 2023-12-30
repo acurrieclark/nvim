cedaf9c350ebd0fdef5ce63b70f909b4dff0d2a2
Alex Currie-Clark <acurrieclark@gmail.com>
2023-12-30 21:36:42 +0000

    Additional treesitter config

--- init.lua
+++ init.lua
@@ -513,10 +513,10 @@ vim.keymap.set('n', '<leader>ds', telescope_functions.document_symbols, { desc =
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
+    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'svelte', 'html' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-    auto_install = false,
+    auto_install = true,
 
     highlight = { enable = true },
     indent = { enable = true },
