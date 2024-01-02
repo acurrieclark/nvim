461b7a24e542c98d8dac7278f5335f51cacb42c0
Alex Currie-Clark <acurrieclark@gmail.com>
2024-01-02 09:30:08 +0000

    Simplified formatting setup to avoid duplication

--- lua/custom/plugins/null-ls.lua
+++ lua/custom/plugins/null-ls.lua
@@ -1,4 +1,3 @@
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 return {
   'nvimtools/none-ls.nvim',
   config = function()
@@ -7,29 +6,15 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.prettierd.with({
-          extra_filetypes = { "svelte" },
+          extra_filetypes = { 'svelte' },
         }),
+        null_ls.builtins.formatting.eslint_d,
+
         null_ls.builtins.completion.luasnip,
         null_ls.builtins.completion.tags,
 
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.eslint_d,
-
         null_ls.builtins.diagnostics.php,
-      },
-      -- you can reuse a shared lspconfig on_attach callback here
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format({ bufnr = bufnr })
-            end,
-          })
-        end
-      end,
+      }
     })
   end,
 }
