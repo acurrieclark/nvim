1e89028440f12721e5d543c1de51cb36937934d7
Alex Currie-Clark <acurrieclark@Alexs-MacBook-Pro.local>
2024-12-10 23:07:00 +0000

    keymap to show notification history

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -2,11 +2,22 @@ return {
   'folke/snacks.nvim',
   priority = 1000,
   lazy = false,
+  ---@type snacks.Config
   opts = {
     dashboard = { enabled = true },
     bigfile = { enabled = true },
     notifier = { enabled = true },
     quickfile = { enabled = true },
     words = { enabled = true },
+  },
+  keys = {
+    {
+      '<leader>N',
+      function()
+        local snacks = require 'snacks'
+        snacks.notifier.show_history()
+      end,
+      desc = 'Notification History',
+    },
   },
 }
