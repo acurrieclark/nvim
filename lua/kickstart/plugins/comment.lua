09cf2aaedde80b8ebac47486b294fba949b0f0c7
Alex Currie-Clark <acurrieclark@Alexs-MacBook-Pro.local>
2024-12-13 14:40:53 +0000

    Reinstate comment.nvim

--- lua/kickstart/plugins/comment.lua
+++ lua/kickstart/plugins/comment.lua
@@ -1,13 +1,22 @@
 -- "gc" to comment visual regions/lines
 return {
-  'JoosepAlviste/nvim-ts-context-commentstring',
+  'numToStr/Comment.nvim',
   opts = {
-    custom_calculation = function(_, language_tree)
-      if vim.bo.filetype == 'blade' and language_tree._lang ~= 'javascript' then
-        return '{{-- %s --}}'
-      elseif (vim.bo.filetype == 'antlers.php' or vim.bo.filetype == 'antlers.html') and language_tree._lang ~= 'javascript' then
-        return '{{# %s #}}'
-      end
+    pre_hook = function()
+      return vim.bo.commentstring
     end,
+  },
+  -- context dependent comments in files like svelte
+  dependencies = {
+    'JoosepAlviste/nvim-ts-context-commentstring',
+    opts = {
+      custom_calculation = function(_, language_tree)
+        if vim.bo.filetype == 'blade' and language_tree._lang ~= 'javascript' then
+          return '{{-- %s --}}'
+        elseif (vim.bo.filetype == 'antlers.php' or vim.bo.filetype == 'antlers.html') and language_tree._lang ~= 'javascript' then
+          return '{{# %s #}}'
+        end
+      end,
+    },
   },
 }
